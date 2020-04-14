4f203babd2bdb36c3ba5e58af50224fe62021d67
Mohit Bajaj <mohit_b35@hotmail.com>
2020-04-14 15:25:05 +0530

    pass search term to App (parent component) 1. App converted to class based component 2. onSearchSubmit() defined for App, and passed as prop to SearchBar 3. OnSearchSubmit invoked in SearchBar onFormSubmit event handler

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,12 +4,29 @@ import './App.css'
 
 import SearchBar from './SearchBar';
 
-const App = () => {
+class App extends React.Component {
+	onSearchSubmit(term) {
+		console.log(term);
+	}
+
+
+	render() {
+		return (
+			<div className="ui container app">
+				<SearchBar onSubmit={this.onSearchSubmit}/>
+			</div>
+		)
+	}
+}
+
+
+// Functional component - converted to class based component, so the commented out code is discarded.
+/* const App = () => {
 	return (
 		<div className="ui container app">
-			<SearchBar />
+			<SearchBar on/>
 		</div>
 	)
-}
+} */
 
 export default App;
