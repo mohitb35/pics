61b91728f9d3a550b4b5c339cf4b9ad3ccefe125
Mohit Bajaj <mohit_b35@hotmail.com>
2020-04-14 11:08:40 +0530

    Scaffolding 1. create /Components to house Component js and css 2. Basic linking of App and SearchBar and rendering in browser 3. SearchBar.js - basic search form 4. Initial styling - semantic UI and custom CSS

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css'
+
+import SearchBar from './SearchBar';
+
+const App = () => {
+	return (
+		<div className="ui container app">
+			<SearchBar />
+		</div>
+	)
+}
+
+export default App;
