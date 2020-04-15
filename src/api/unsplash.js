6cb9aba276f33799fac6d220679bbc05c34c880b
Mohit Bajaj <mohit_b35@hotmail.com>
2020-04-15 17:45:10 +0530

    minor changes to config

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import config from '../config';
 
 export default axios.create({
 	baseURL: 'https://api.unsplash.com',
 	headers: {
-		Authorization: 'Client-ID NcV8kR20uZg3pTZn0tNqqijNC36HNGxolDe0K9ywrgA'
+		Authorization: `Client-ID ${config.unsplashClientId}`
 	}
 })
